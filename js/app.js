42b5ca9a08a38788a66ed0449fb0d447d2d33254
julianabaicar <baicarjuliana@gmail.com>
2025-02-15 00:30:17 -0300

    feat: Operadores matemáticos e variáveis.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,35 @@
 console.log('Olá, Javascript!')
+
+
+var userName = 'Mana Ju'
+
+// obter um elemento htlm com o java script por meio do ID
+document.getElementById('user-name').innerHTML = userName
+
+
+// EXEMPLOS:
+    // variaveis em js -> possui uma tipagem dinâmica
+    // var nome  = 'Mestre Yoda'
+    // var idade = 100
+    // var jedi  = true
+
+    // typeof mostra o tipo, se chamar só o nome ele pega o conteúdo
+    // console.log(typeof nome)
+    // console.log(typeof idade)
+    // console.log(typeof jedi)
+
+
+// Operadores matemáticos //
+    // + para somar
+    //  - para subtrair
+    // * para multiplicar
+    //  / para dividir
+
+var n1 = 5
+var n2 = 2.5
+
+console.log(typeof n1)
+console.log(typeof n2)
+
+var total = n1 / n2
+console.log(total)
