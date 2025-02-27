d4d251a91e1b6b82c4a2fb4eef0605db69dbca2e
julianabaicar <baicarjuliana@gmail.com>
2025-02-27 09:41:24 -0300

    feat: create object with vueJs and change ids for object in html.

---
+++ js/app.js
@@ -0,0 +1,9 @@
+
+// regra do vue o nome dessa constante estar em maiusculo
+const App = new Vue({
+    el: '#app',
+    data: {
+        title: 'Star Wars Lego',
+        userName: 'Juliana'
+    }
+})
